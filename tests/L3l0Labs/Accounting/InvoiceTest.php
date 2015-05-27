fbc7985db3e542ec164a028486c728b6f0c28aae
l3l0 <leszek.prabucki@gmail.com>
2015-05-27 15:53:15 +0200

    Planning invoice related value objects.

--- tests/L3l0Labs/Accounting/InvoiceTest.php
+++ tests/L3l0Labs/Accounting/InvoiceTest.php
@@ -3,7 +3,6 @@
 namespace Tests\L3l0Labs\Accounting;
 
 use L3l0Labs\Accounting\Invoice;
-use L3l0Labs\Accounting\Invoice\Item;
 
 class InvoiceTest extends \PhpUnit_Framework_TestCase
 {
@@ -14,29 +13,36 @@ class InvoiceTest extends \PhpUnit_Framework_TestCase
     {
         $invoice = new Invoice(
             $number = '2015/01/01',
-            $sellerName = 'Cocoders Sp. z o.o',
-            $sellerAddress = 'ul. Jęczmienna 19, 87-200 Toruń',
-            $sellerVatNumber = '9562307984',
-            $issueDate = new \DateTime('2015-01-01'),
-            $maturityDate = new \DateTime('2015-01-07'),
+            new Invoice\Seller(
+                'Cocoders Sp. z o.o',
+                'ul. Jęczmienna 19, 87-200 Toruń',
+                '9562307984'
+            ),
+            new Invoice\Period(
+                new \DateTime('2015-01-01'),
+                new \DateTime('2015-01-07')
+            ),
             $sellDate = new \DateTime('2015-01-01'),
-            $buyerName = 'Leszek Prabucki "l3l0 labs"',
-            $buyerAddress = 'ul. Królewskie Wzgórze 21/9, 80-283 Gdańsk',
-            $buyerVatNumber = '9562307984'
+            new Invoice\Buyer(
+                'Leszek Prabucki "l3l0 labs"',
+                'ul. Królewskie Wzgórze 21/9, 80-283 Gdańsk',
+                '9562307984'
+            )
         );
         $invoice->setAdditionalText('test');
 
-        $item = new Item(
+        $item = new Invoice\Item(
             'Wytwarzanie aplikacji internetowych',
             $quantity = 1,
             $unitNetPrice = 3000,
             $vatRate = 0.23
         );
-
-        $invoice->setItems([$item]);
+        $invoice->addItem($item);
 
         $this->assertEquals('2015/01/01', $invoice->getNumber());
-        $this->assertEquals('Cocoders Sp. z o.o', $invoice->getSellerName());
-        $this->assertEquals('ul. Jęczmienna 19, 87-200 Toruń', $invoice->getSellerAddress());
+        $this->assertEquals('Cocoders Sp. z o.o', $invoice->getSeller()->getName());
+        $this->assertEquals('ul. Jęczmienna 19, 87-200 Toruń', $invoice->getSeller()->getAddress());
+        $this->assertEquals('Leszek Prabucki "l3l0 labs"', $invoice->getBuyer()->getName());
+        $this->assertEquals('ul. Królewskie Wzgórze 21/9, 80-283 Gdańsk', $invoice->getBuyer()->getAddress());
     }
 }
